9a9d7c9d70b8c17411dd42d2b9e55c8a80859f6d
yanxiaojun617 <yanxiaojun617@163.com>
2019-08-15 14:12:51 +0800

    feat：允许跨域 fix: swagger-ui.html找不到页面问题

---
+++ src/main/java/com/akm/springboot/core/config/WebMvcConfig.java
@@ -0,0 +1,33 @@
+package com.akm.springboot.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 在这里进行Spring MVC的配置, 如 interceptor, messageConverter, formatter, viewResolver等
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .maxAge(3600)
+                .allowCredentials(false);
+    }
+
+}
